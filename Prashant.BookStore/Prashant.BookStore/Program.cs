fb459ffc607c0814c6c7aeeee80be66f06851125
Prashant Sharma <prashant23sharma@gmail.com>
2023-05-31 12:11:49 +0530

    new changes

--- Prashant.BookStore/Prashant.BookStore/Program.cs
+++ Prashant.BookStore/Prashant.BookStore/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+// REGISTER SERVICES HERE
 var app = builder.Build();
+// REGISTER MIDDLEWARE HERE
 
-app.MapGet("/", () => "Hello World!");
+//5app.MapGet("/", () => "Hello World!");
+
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Hello from my first middleware");
+//});
 
 app.Run();
+
